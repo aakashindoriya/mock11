1d276ea2f590ef0b2c3bff85d533efcb6b854ae6
aakash <37771235+aakashindoriya@users.noreply.github.com>
2023-01-19 15:49:57 +0530

    auth added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ let cors=require("cors")
 let connect=require("./config/database")
 const UserRouter=require("./routes/user.routes")
 const BugRouter=require("./routes/bug.routes")
+const { request } = require("express")
+const Auth=require("./middleware/Auth.mddleware")
 const app=express()
 app.use(express.json())
 app.use(cors())
 
 app.use("/user",UserRouter)
-app.use("/bugs",BugRouter)
+app.use("/bugs",Auth,BugRouter)
 
 app.listen(process.env.PORT,()=> {
     connect()
